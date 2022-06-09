603d0ead81f80e580d7afd80dee35dd030eeb239
김만규 <private.narc2ss@gmail.com>
2022-06-09 20:48:02 +0900

    Feature/3 (#2)
    * feature : function2를 추가한다
    
    * feature : function3을 추가한다

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 function function1() {
   console.log("called function1");
 }
+function function2() {
+  console.log("called function2");
+}
+function function3() {
+  console.log("called function3");
+}
 
 function1();
+function2();
+function3();
