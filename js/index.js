1a2e87658b7e1bbc71141ae283a4adacf2da60de
narc2ss <private.narc2ss@gmail.com>
2022-06-09 20:45:35 +0900

    feature : function3을 추가한다

--- js/index.js
+++ js/index.js
@@ -4,6 +4,10 @@ function function1() {
 function function2() {
   console.log("called function2");
 }
+function function3() {
+  console.log("called function3");
+}
 
 function1();
 function2();
+function3();
